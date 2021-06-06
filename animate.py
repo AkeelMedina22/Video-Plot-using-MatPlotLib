3fc155a5ff8219a18c52e37035b7b497612d1a54
Akeel Ather Medina <56905673+AkeelMedina22@users.noreply.github.com>
2021-06-06 08:13:22 +0500

    Update animate.py

--- animate.py
+++ animate.py
@@ -22,10 +22,15 @@ line = plt.scatter([0], [0], s=0.1, c='black')
 
 def animate(i):
     im = frames[i]
-    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    gray_img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    
+    # For black background videos, lower the threshold near 75.
+    # For white background videos, increase to max 255, near 200
+    _, img = cv2.threshold(gray_img, 150, 150, cv2.THRESH_BINARY_INV)
+    img = cv2.medianBlur(img, 1)
+
     contours, hierarchy = cv2.findContours(
-        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     hierarchy = hierarchy[0]
 
